e76f6b8324268cb7e9a3aa8e503f762a8b6b55c5
Tharunda De Silva <tharundadesilva@gmail.com>
2025-08-03 15:58:44 +0530

    product list added

---
+++ Product.js
@@ -0,0 +1,63 @@
+let products = [
+    {
+        name:"HP 250 G7 | i5 8th Gen | 256GB SSD | 8GB RAM DDR4",
+        price:78000,
+        image:"https://computerstore.lk/wp-content/uploads/2025/06/HP-250-G7.jpg"
+
+    },
+    {
+        name:"MSI PRO MP251L E2 24.5",
+        price:32000,
+        image:"https://computerstore.lk/wp-content/uploads/2025/04/MP251L-E2-1.jpg"
+
+    },
+    {
+        name:"MSI CLUTCH GM11 GAMING MOUSE ",
+        price:11000,
+        image:"https://computerstore.lk/wp-content/uploads/2024/01/MSI-CLUTCH-GM11-GAMING-MOUSE.jpg"
+
+    },
+    {
+        name:"REDRAGON KUMARA PRO K552P MECHANICAL KEYBOARD",
+        price:13000,
+        image:"https://computerstore.lk/wp-content/uploads/2024/08/REDRAGON-KUMARA-PRO-K552P-MECHANICAL-KEYBOARD-2.jpg"
+
+    },
+    {
+        name:"Lexar SSD 1tb M.2 NVME Nm620",
+        price:"22500",
+        image:"https://computerstore.lk/wp-content/uploads/2024/01/Ssd-Lexar-1tb-M.2-Nvme-Nm620.jpg"
+
+    },
+    {
+        name:"HP 250 G7 | i5 8th Gen | 256GB SSD | 8GB RAM DDR4",
+        price:78000,
+        image:"https://computerstore.lk/wp-content/uploads/2025/06/HP-250-G7.jpg"
+
+    },
+    {
+        name:"MSI PRO MP251L E2 24.5",
+        price:32000,
+        image:"https://computerstore.lk/wp-content/uploads/2025/04/MP251L-E2-1.jpg"
+
+    },
+    {
+        name:"MSI CLUTCH GM11 GAMING MOUSE ",
+        price:11000,
+        image:"https://computerstore.lk/wp-content/uploads/2024/01/MSI-CLUTCH-GM11-GAMING-MOUSE.jpg"
+
+    },
+    {
+        name:"REDRAGON KUMARA PRO K552P MECHANICAL KEYBOARD",
+        price:13000,
+        image:"https://computerstore.lk/wp-content/uploads/2024/08/REDRAGON-KUMARA-PRO-K552P-MECHANICAL-KEYBOARD-2.jpg"
+
+    },
+    {
+        name:"Lexar SSD 1tb M.2 NVME Nm620",
+        price:"22500",
+        image:"https://computerstore.lk/wp-content/uploads/2024/01/Ssd-Lexar-1tb-M.2-Nvme-Nm620.jpg"
+
+    },
+    
+]
